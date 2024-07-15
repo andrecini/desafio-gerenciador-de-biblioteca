0552cc9fc45f03c05478794ed7601c58276e3014
André Cini <andrevpcini@gmail.com>
2024-07-15 18:04:38 -0300

    [FEAT] - Atualização das migrations

--- src/Infra/Desafios.GerenciadorBiblioteca.Data/Migrations/20240715204122_v1.Designer.cs
+++ src/Infra/Desafios.GerenciadorBiblioteca.Data/Migrations/20240715204122_v1.Designer.cs
@@ -4,6 +4,7 @@ using Desafios.GerenciadorBiblioteca.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Desafios.GerenciadorBiblioteca.Data.Migrations
 {
     [DbContext(typeof(LibraryDbContext))]
-    partial class LibraryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240715204122_v1")]
+    partial class v1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
