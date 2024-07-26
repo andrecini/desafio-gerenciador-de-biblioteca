37b0e1c73524f29bb8105608e28581b2e6b385b0
André Cini <andrevpcini@gmail.com>
2024-07-26 19:57:41 -0300

    [FEAT] - Inicialização da lógica de esconder funcionalidades de admins

--- src/Presentation/Desafios.GerenciadorBiblioteca.Front/Services/Auth/AuthService.cs
+++ src/Presentation/Desafios.GerenciadorBiblioteca.Front/Services/Auth/AuthService.cs
@@ -1,4 +1,4 @@
-﻿using Desafios.GerenciadorBiblioteca.Service.DTOs.ViewModels;
+﻿using Desafios.GerenciadorBiblioteca.Domain.Enums;
 using Desafios.GerenciadorBiblioteca.Website.Models.Responses;
 using Microsoft.AspNetCore.Components.Authorization;
 
@@ -14,12 +14,42 @@ namespace Desafios.GerenciadorBiblioteca.Website.Services.Auth
             await _tokenService.SetTokenIdentity(tokenViewModel);
             await _authenticationStateProvider.GetAuthenticationStateAsync();
         }
-        public async Task Logout(TokenModel tokenViewModel)
+
+        public async Task Logout()
         {
             await _tokenService.ClearTokenIdentity();
             await _authenticationStateProvider.GetAuthenticationStateAsync();
         }
 
-       
+        public async Task<bool> VerifyUserSessionIsOpenAsync()
+        {
+            try
+            {
+                var token = await _tokenService.GetTokenIdentity();
+
+                if (token == null)
+                    return false;
+
+                if (token.ValidTo >= DateTime.Now)
+                {
+                    token.ValidTo = DateTime.Now;
+                    await Login(token);
+                    return true;
+                }
+
+                await _tokenService.ClearTokenIdentity();
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> VerifyIfUserIsAdmin()
+        {
+            var token = await _tokenService.GetTokenIdentity();
+            return token.Role == Roles.Administrator;
+        }
     }
 }
