d89ee1666c6a34bfd722b413a828e8b81b543110
André Cini <andrevpcini@gmail.com>
2024-07-30 00:23:21 -0300

    [CHORE] - Ajuste na estruturação dos projetos
    Issue: #11

--- src/Infra/Desafios.GerenciadorBiblioteca.Data/Migrations/20240730032026_v2.Designer.cs
+++ src/Infra/Desafios.GerenciadorBiblioteca.Data/Migrations/20240730032026_v2.Designer.cs
@@ -4,6 +4,7 @@ using Desafios.GerenciadorBiblioteca.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Desafios.GerenciadorBiblioteca.Data.Migrations
 {
     [DbContext(typeof(LibraryDbContext))]
-    partial class LibraryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240730032026_v2")]
+    partial class v2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
